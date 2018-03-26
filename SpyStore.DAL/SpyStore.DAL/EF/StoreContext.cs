81ce801d001e007cefbefedcda4997b9a312a899
SilentBomber7 <ace71182@gmail.com>
2018-03-26 13:03:54 -0500

    Assignment 6

---
+++ SpyStore.DAL/SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SpyStore.Models;
+using SpyStore.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpyStore.DAL.EF
+{
+    public class StoreContext : DbContext
+    {
+        private static DbContextOptions options;
+
+        public StoreContext()
+
+        {
+        }
+        public StoreContext(DbContextOptions options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(lovaldb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSets=true;");
+            }
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        
+    }
+}
---
+++ SpyStore.DAL/SpyStore.DAL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SpyStore.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            class Program
+        {
+            static void Main(string[] args)
+        }
+        }
+    }
+
